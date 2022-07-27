bdf816330250247b7218fa8f8f3305db7ce52fb5
Jasper Thrussell <jthrussell@lanl.gov>
2022-07-27 14:19:15 -0600

    Minor change to test module docstring

--- exactpack/tests/__init__.py
+++ exactpack/tests/__init__.py
@@ -1,8 +1,9 @@
 """Unit testing for ExactPack.
 
 Although (or, perhaps, because) one important use case for ExactPack
-is for generating solutions to use in verification of other codes,
-ExactPack itself needs to be tested.  The modules in
-:mod:`exactpack.test` contain python :py:mod:`unittest` test cases
-designed to self-test the library.
+is for generating solutions to verify a general hydro code, ExactPack
+itself needs to be tested.  The modules in :mod:`exactpack.test` contain
+python :py:mod:`unittest` test cases designed to self-test the library.
+To run a specific test, say for the Noh solver, type "python -m unittest
+exactpack.tests.test_noh".
 """
