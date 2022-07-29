7178d349aff1f0a64af6a72643ccad440c4aa022
Jasper Thrussell <jthrussell@lanl.gov>
2022-07-29 15:08:12 -0600

    Re-enable tests for the SuOlson solver

--- exactpack/tests/test_suolson.py
+++ exactpack/tests/test_suolson.py
@@ -5,25 +5,25 @@ import unittest
 
 import numpy
 
-# This will fail to import because the suolsen solver won't build
-# from exactpack.solvers.suolson.timmes import SuOlson
+from exactpack.solvers.suolson.timmes import SuOlson
+
 
 class TestSuOlsonTimmes(unittest.TestCase):
     r"""Regresstion tests for :class:`exactpack.solvers.suolson.timmes.SuOlson`.
 
     Comparisions are made at :math:`r=0.1` and :math:`t=10^{-9}`.
     """
-    @unittest.expectedFailure
     def test_radiation_temperature(self):
         """SuOlson problem: radiation temperature"""
 
-        self.assertAlmostEqual(SuOlson(trad_bc_ev=1.0e3, opac=1.0)(numpy.array([0.1]), 1.e-9).Tradiation[0], 949.7484176757317)
+        result = SuOlson(trad_bc_ev=1.0e3, opac=1.0)(numpy.array([0.1]), 1.e-9).Tradiation[0]
+        self.assertAlmostEqual(result, 949.7484176757317)
 
-    @unittest.expectedFailure
     def test_matter_temperature(self):
         """SuOlson problem: matter temperature"""
 
-        self.assertAlmostEqual(SuOlson(trad_bc_ev=1.0e3, opac=1.0)(numpy.array([0.1]), 1.e-9).Tmaterial[0], 948.8608008525508)
+        result = SuOlson(trad_bc_ev=1.0e3, opac=1.0)(numpy.array([0.1]), 1.e-9).Tmaterial[0]
+        self.assertAlmostEqual(result, 948.8608008525508)
 
 
         
