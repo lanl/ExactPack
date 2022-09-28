94ec6e483fe021787ece99eca8857697e4f410a5
Jasper Thrussell <jthrussell@lanl.gov>
2022-09-28 11:35:15 -0600

    Fix flake8 errors in interp_laz

--- exactpack/solvers/guderley/interp_laz.py
+++ exactpack/solvers/guderley/interp_laz.py
@@ -30,14 +30,13 @@ def interp_laz(n, gamma, lambda_):
         raise ValueError('Invalid geometry')
 
     bhat = DO_INTERP(lcyl, gamma, lambda_)
-
-    #.... Strictly using "bhat" as the upper bound for the more precise 
-    #	value of bhat will in all likeihood produce a "endpoints do not
-    #	have opposite signs" error in the "zeroin" routine used in the
-    #	"guderley_1D driving program. Therefore, the interpolated value
-    #	of bhat is adjusted slightly to counter this error. The setting 
-    #	at which it is currently placed may need to be adjusted for 
-    # 	various cases of gamma and n.
+    # Strictly using "bhat" as the upper bound for the more precise
+    # value of bhat will in all likeihood produce a "endpoints do not
+    # have opposite signs" error in the "zeroin" routine used in the
+    # "guderley_1D driving program. Therefore, the interpolated value
+    # of bhat is adjusted slightly to counter this error. The setting
+    # at which it is currently placed may need to be adjusted for
+    # various cases of gamma and n.
     return bhat
 
 
@@ -49,12 +48,10 @@ def DO_INTERP(lcyl, gamma, lambda_):
         gamval, lamval, bval = GET_CYL()
     else:
         gamval, lamval, bval = GET_SPH()
-
     # Orignal code has lamvals and lambda, we assume someone might want to
     # interpolate lambda?
     # tck = interpolate.splrep(lamval, bval)
     # bhat = interpolate.splev(lambda_, tck)
-
     tck = interpolate.splrep(gamval, bval)
     bhat = interpolate.splev(gamma, tck)
 
@@ -70,63 +67,63 @@ def GET_CYL():
     lamval = np.zeros(nmax)
     bval = np.zeros(nmax)
 
-    gamval[ 0] = 1.00001
-    gamval[ 1] = 1.0001 
-    gamval[ 2] = 1.001  
-    gamval[ 3] = 1.005  
-    gamval[ 4] = 1.01   
-    gamval[ 5] = 1.03   
-    gamval[ 6] = 1.05   
-    gamval[ 7] = 1.07   
-    gamval[ 8] = 1.10   
-    gamval[ 9] = 1.15   
-    gamval[10] = 1.2    
-    gamval[11] = 1.3    
-    gamval[12] = 1.4    
-    gamval[13] = 1.5    
+    gamval[0] = 1.00001
+    gamval[1] = 1.0001
+    gamval[2] = 1.001
+    gamval[3] = 1.005
+    gamval[4] = 1.01
+    gamval[5] = 1.03
+    gamval[6] = 1.05
+    gamval[7] = 1.07
+    gamval[8] = 1.10
+    gamval[9] = 1.15
+    gamval[10] = 1.2
+    gamval[11] = 1.3
+    gamval[12] = 1.4
+    gamval[13] = 1.5
     gamval[14] = 1.66667
-    gamval[15] = 1.7    
-    gamval[16] = 1.8    
-    gamval[17] = 1.9    
-    gamval[18] = 1.92   
-    gamval[19] = 2.0    
-    gamval[20] = 2.0863 
-    gamval[21] = 2.0883 
-    gamval[22] = 2.125  
-    gamval[23] = 2.2    
-    gamval[24] = 2.3676 
-    gamval[25] = 2.3678 
-    gamval[26] = 2.4    
-    gamval[27] = 2.6    
-    gamval[28] = 2.8    
+    gamval[15] = 1.7
+    gamval[16] = 1.8
+    gamval[17] = 1.9
+    gamval[18] = 1.92
+    gamval[19] = 2.0
+    gamval[20] = 2.0863
+    gamval[21] = 2.0883
+    gamval[22] = 2.125
+    gamval[23] = 2.2
+    gamval[24] = 2.3676
+    gamval[25] = 2.3678
+    gamval[26] = 2.4
+    gamval[27] = 2.6
+    gamval[28] = 2.8
     gamval[29] = 2.83920
     gamval[30] = 2.83929
-    gamval[31] = 3.0    
-    gamval[32] = 3.4    
-    gamval[33] = 4.0    
-    gamval[34] = 5.0    
-    gamval[35] = 6.0    
-    gamval[36] = 7.0    
-    gamval[37] = 8.0    
-    gamval[38] = 10.0   
-    gamval[39] = 15.0   
-    gamval[40] = 20.0   
-    gamval[41] = 30.0   
-    gamval[42] = 50.0   
-    gamval[43] = 100.0  
-    gamval[44] = 1000.0 
+    gamval[31] = 3.0
+    gamval[32] = 3.4
+    gamval[33] = 4.0
+    gamval[34] = 5.0
+    gamval[35] = 6.0
+    gamval[36] = 7.0
+    gamval[37] = 8.0
+    gamval[38] = 10.0
+    gamval[39] = 15.0
+    gamval[40] = 20.0
+    gamval[41] = 30.0
+    gamval[42] = 50.0
+    gamval[43] = 100.0
+    gamval[44] = 1000.0
     gamval[45] = 9999.0
 
-    lamval[ 0] = 1.0022073240
-    lamval[ 1] = 1.0068195769
-    lamval[ 2] = 1.0202846866
-    lamval[ 3] = 1.0414733956
-    lamval[ 4] = 1.0553973808
-    lamval[ 5] = 1.0850737604
-    lamval[ 6] = 1.1023892512
-    lamval[ 7] = 1.1150692073
-    lamval[ 8] = 1.1296268597
-    lamval[ 9] = 1.1475773258
+    lamval[0] = 1.0022073240
+    lamval[1] = 1.0068195769
+    lamval[2] = 1.0202846866
+    lamval[3] = 1.0414733956
+    lamval[4] = 1.0553973808
+    lamval[5] = 1.0850737604
+    lamval[6] = 1.1023892512
+    lamval[7] = 1.1150692073
+    lamval[8] = 1.1296268597
+    lamval[9] = 1.1475773258
     lamval[10] = 1.1612203175
     lamval[11] = 1.1817213587
     lamval[12] = 1.1971414294
@@ -164,16 +161,16 @@ def GET_CYL():
     lamval[44] = 1.3751432790
     lamval[45] = 1.3753967176
 
-    bval[ 0] = 0.521740
-    bval[ 1] = 0.554609
-    bval[ 2] = 0.625514
-    bval[ 3] = 0.697737
-    bval[ 4] = 0.724429
-    bval[ 5] = 0.731819
-    bval[ 6] = 0.708880
-    bval[ 7] = 0.682234
-    bval[ 8] = 0.644590
-    bval[ 9] = 0.593262
+    bval[0] = 0.521740
+    bval[1] = 0.554609
+    bval[2] = 0.625514
+    bval[3] = 0.697737
+    bval[4] = 0.724429
+    bval[5] = 0.731819
+    bval[6] = 0.708880
+    bval[7] = 0.682234
+    bval[8] = 0.644590
+    bval[9] = 0.593262
     bval[10] = 0.554542
     bval[11] = 0.502117
     bval[12] = 0.469268
@@ -223,69 +220,69 @@ def GET_SPH():
     lamval = np.zeros(nmax)
     bval = np.zeros(nmax)
 
-    gamval[ 0] = 1.00001
-    gamval[ 1] = 1.0001 
-    gamval[ 2] = 1.001  
-    gamval[ 3] = 1.01   
-    gamval[ 4] = 1.03   
-    gamval[ 5] = 1.05   
-    gamval[ 6] = 1.07   
-    gamval[ 7] = 1.10   
-    gamval[ 8] = 1.15   
-    gamval[ 9] = 1.2    
-    gamval[10] = 1.3    
-    gamval[11] = 1.4    
-    gamval[12] = 1.5    
-    gamval[13] = 1.6    
+    gamval[0] = 1.00001
+    gamval[1] = 1.0001
+    gamval[2] = 1.001
+    gamval[3] = 1.01
+    gamval[4] = 1.03
+    gamval[5] = 1.05
+    gamval[6] = 1.07
+    gamval[7] = 1.10
+    gamval[8] = 1.15
+    gamval[9] = 1.2
+    gamval[10] = 1.3
+    gamval[11] = 1.4
+    gamval[12] = 1.5
+    gamval[13] = 1.6
     gamval[14] = 1.66667
-    gamval[15] = 1.7    
-    gamval[16] = 1.8    
-    gamval[17] = 1.86   
-    gamval[18] = 1.88   
-    gamval[19] = 1.9    
-    gamval[20] = 2.0    
-    gamval[21] = 2.010  
-    gamval[22] = 2.012  
-    gamval[23] = 2.2    
-    gamval[24] = 2.2215 
-    gamval[25] = 2.2217 
-    gamval[26] = 2.4    
-    gamval[27] = 2.5518 
+    gamval[15] = 1.7
+    gamval[16] = 1.8
+    gamval[17] = 1.86
+    gamval[18] = 1.88
+    gamval[19] = 1.9
+    gamval[20] = 2.0
+    gamval[21] = 2.010
+    gamval[22] = 2.012
+    gamval[23] = 2.2
+    gamval[24] = 2.2215
+    gamval[25] = 2.2217
+    gamval[26] = 2.4
+    gamval[27] = 2.5518
     gamval[28] = 2.55194
-    gamval[29] = 2.6    
-    gamval[30] = 2.8    
-    gamval[31] = 3.0    
-    gamval[32] = 3.2    
-    gamval[33] = 3.4    
-    gamval[34] = 3.6    
-    gamval[35] = 3.8    
-    gamval[36] = 4.0    
-    gamval[37] = 4.5    
-    gamval[38] = 5.0    
-    gamval[39] = 5.5    
-    gamval[40] = 6.0    
-    gamval[41] = 6.5    
-    gamval[42] = 7.0    
-    gamval[43] = 8.0    
-    gamval[44] = 10.0   
-    gamval[45] = 15.0   
-    gamval[46] = 20.0   
-    gamval[47] = 30.0   
-    gamval[48] = 50.0   
-    gamval[49] = 100.0  
-    gamval[50] = 1000.0 
-    gamval[51] = 9999.0 
+    gamval[29] = 2.6
+    gamval[30] = 2.8
+    gamval[31] = 3.0
+    gamval[32] = 3.2
+    gamval[33] = 3.4
+    gamval[34] = 3.6
+    gamval[35] = 3.8
+    gamval[36] = 4.0
+    gamval[37] = 4.5
+    gamval[38] = 5.0
+    gamval[39] = 5.5
+    gamval[40] = 6.0
+    gamval[41] = 6.5
+    gamval[42] = 7.0
+    gamval[43] = 8.0
+    gamval[44] = 10.0
+    gamval[45] = 15.0
+    gamval[46] = 20.0
+    gamval[47] = 30.0
+    gamval[48] = 50.0
+    gamval[49] = 100.0
+    gamval[50] = 1000.0
+    gamval[51] = 9999.0
 
-    lamval[ 0] = 1.0044047883
-    lamval[ 1] = 1.0135647885
-    lamval[ 2] = 1.0401005736
-    lamval[ 3] = 1.1088100742
-    lamval[ 4] = 1.1671691602
-    lamval[ 5] = 1.2015664277
-    lamval[ 6] = 1.2269581432
-    lamval[ 7] = 1.2563291060
-    lamval[ 8] = 1.2928404943
-    lamval[ 9] = 1.3207565353
+    lamval[0] = 1.0044047883
+    lamval[1] = 1.0135647885
+    lamval[2] = 1.0401005736
+    lamval[3] = 1.1088100742
+    lamval[4] = 1.1671691602
+    lamval[5] = 1.2015664277
+    lamval[6] = 1.2269581432
+    lamval[7] = 1.2563291060
+    lamval[8] = 1.2928404943
+    lamval[9] = 1.3207565353
     lamval[10] = 1.3628123548
     lamval[11] = 1.3943607838
     lamval[12] = 1.4195913539
@@ -329,16 +326,16 @@ def GET_SPH():
     lamval[50] = 1.6994953607
     lamval[51] = 1.6998093041
 
-    bval[ 0] = 0.541777
-    bval[ 1] = 0.607335
-    bval[ 2] = 0.758422
-    bval[ 3] = 0.988008
-    bval[ 4] = 0.996617
-    bval[ 5] = 0.931071
-    bval[ 6] = 0.860781
-    bval[ 7] = 0.769242
-    bval[ 8] = 0.658324
-    bval[ 9] = 0.584657
+    bval[0] = 0.541777
+    bval[1] = 0.607335
+    bval[2] = 0.758422
+    bval[3] = 0.988008
+    bval[4] = 0.996617
+    bval[5] = 0.931071
+    bval[6] = 0.860781
+    bval[7] = 0.769242
+    bval[8] = 0.658324
+    bval[9] = 0.584657
     bval[10] = 0.496984
     bval[11] = 0.448082
     bval[12] = 0.417547
