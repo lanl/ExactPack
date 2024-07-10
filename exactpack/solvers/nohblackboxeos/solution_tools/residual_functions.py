Fixed test_nohblackbox.py; wrong value for test_F_value, wrong error name for test determinant.

exactpack/solvers/nohblackboxeos/solution_tools/residual_functions.py
@@ -139,7 +139,7 @@ class noh_residual(newton_solver_residual_function):
         temp = self.F_prime([rho, P,D]) # Computes F_prime 
         det = self.determinant(temp) # Computes the determinant of F_prime
         if(det == 0): # Check that determinant isn't 0; helps to gurantee that the inverse exists
-            raise ValueError("Zero determinant; inverse does not exist. Exiting iteration.")
+            raise ZeroDeterminantError("Zero determinant; inverse does not exist. Exiting iteration.")
         self.DF_inv = np.linalg.inv(temp) # Invert F_prime 
         return self.DF_inv
 

exactpack/tests/test_nohblackbox.py
@@ -141,7 +141,7 @@ class TestResidualFunction:
 
     def test_F_value(self): 
         function = noh_residual(self.value_test_ic, self.eos)
-        np.testing.assert_equal(function.F([1,1,1]), [1., 1., -1.5])
+        np.testing.assert_equal(function.F([1,1,1]), [-1., -1., -1.5])
         """Noh Problem--Residual Function: residual function value"""
 
     def test_F_prime_value(self): 